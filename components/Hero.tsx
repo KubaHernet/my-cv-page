576920d5c26102e63c4d5a558f51696d1e071796
Kuba Hernet <jakub.hernet@gmail.com>
2023-07-24 09:46:23 +0200

    change circles to elypsis add hero buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {};
 
@@ -21,18 +22,37 @@ const Hero = () => {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <Image
-        unoptimized
-        src="/me.jpg"
-        alt="me"
-        width="32"
-        height="32"
-        className="relative rounded-full h-32 w-32 mx-auto object-cover"
-      />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <div className="z-20">
+        <Image
+          unoptimized
+          src="/me.jpg"
+          alt="me"
+          width="32"
+          height="32"
+          className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        />
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[10px]">
+          Full-Stack Software Engineer
+        </h2>
+        <h1 className="text-5xl font-semibold scroll-px-10">
+          <span>{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div>
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
