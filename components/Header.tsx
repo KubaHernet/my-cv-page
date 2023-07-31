1bd4ffc37fd56106c0c9e46611e584d549f88adb
Kuba Hernet <jakub.hernet@gmail.com>
2023-07-31 10:18:06 +0200

    add full name to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,9 +10,9 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className="sticky top-0 px-5 py-2 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+    <header className="sticky top-0 px-5 py-2 flex items-start  max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div
-        className="flex flex-row items-center"
+        className="flex flex-1 justify-start flex-row items-center"
         initial={{
           x: -500,
           opacity: 0,
@@ -30,7 +30,29 @@ const Header = (props: Props) => {
         <HeroMenu />
       </motion.div>
       <motion.div
-        className="flex flex-row items-center text-gray-300"
+        className="flex flex-1 justify-center flex-row items-center"
+        initial={{
+          y: -200,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          y: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+      >
+        <Link href="#hero">
+          <h2 className="font-thin text-2xl">
+            Jakub<span className="font-bold">Hernet</span>
+          </h2>
+        </Link>
+      </motion.div>
+      <motion.div
+        className="flex flex-1 justify-end flex-row items-center text-gray-300"
         initial={{
           x: 500,
           opacity: 0,
